76ac32371fdb33643c55e1acbd7aff5ed6321ac9
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-09-05 09:13:39 +0100

    Added snippet on multi dimensional arrays

--- src/basics.c
+++ src/basics.c
@@ -38,4 +38,15 @@ void random_basics() {
     printf("%d ", ar[i]);
   }
   printf("\n");
+  // multi dimenesional arrays
+  double ident[N][N];
+  int row, col;
+  for (row = 0; row < N; row++)
+    for (col = 0; col < N; col++) {
+      if (row == col) {
+        ident[row][col] = 1.0;
+      } else {
+        ident[row][col] = 0.0;
+      };
+    };
 }
