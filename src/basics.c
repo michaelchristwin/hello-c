fb35532cb929619d0e470d0b90ce5d008df6b89b
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-09-05 09:16:38 +0100

    Added snippet on multi constant arrays

--- src/basics.c
+++ src/basics.c
@@ -1,6 +1,11 @@
 #include "../include/basics.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/* C99 only */
+#define NUM_SUITS 4
+#define NUM_RANKS 13
 #define N 10
 
 int add(int a, int b) { return a + b; }
@@ -49,4 +54,26 @@ void random_basics() {
         ident[row][col] = 0.0;
       };
     };
+
+  bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+  int num_cards, rank, suit;
+  const char rank_code[] = {'2', '3', '4', '5', '6', '7', '8',
+                            '9', 't', 'j', 'q', 'k', 'a'};
+  const char suit_code[] = {'c', 'd', 'h', 's'};
+  srand((unsigned)time(NULL));
+  printf("Enter number of cards in hand: ");
+  scanf("%d", &num_cards);
+  printf("Your hand:");
+  while (num_cards > 0) {
+    suit = rand() % NUM_SUITS;
+    /* picks a random suit */
+    rank = rand() % NUM_RANKS;
+    /* picks a random rank */
+    if (!in_hand[suit][rank]) {
+      in_hand[suit][rank] = true;
+      num_cards--;
+      printf(" %c%c", rank_code[rank], suit_code[suit]);
+    }
+  }
+  printf("\n");
 }
