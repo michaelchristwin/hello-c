6cd074126def131f0ad30e454d8efa7bce871459
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-09-30 16:43:21 +0100

    Snippets on simple io

--- src/drchuck.c
+++ src/drchuck.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-// Read a file
+/*Muliplier's prototype*/
+int mymult(int a, int b);
+
+// Main function
 int main() {
+  int retval;
   char line[1000];
   FILE *hand;
   hand = fopen("romeo.txt", "r");
   while (fgets(line, 1000, hand) != NULL) {
     printf("%s", line);
   }
+  retval = mymult(6, 7);
+}
+
+// Muliplier's function statment
+int mymult(int a, int b) {
+  int c = a * b;
+  return c;
 }
